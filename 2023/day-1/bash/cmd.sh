527ccbfea15cf74111ce30453513c19ef38a35af
John Long <john@128.io>
2024-08-02 22:35:33 -0500

    day-1: functional bash implementation

---
+++ 2023/day-1/bash/cmd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SUM=0
+
+while read line; do
+  # Strip all alpha characters
+  NUMBERS=$(echo $line | tr -d '[:alpha:]')
+
+  # Get the first and last number
+  NUMBER=${NUMBERS:0:1}${NUMBERS: -1}
+
+  # Add the number to the sum
+  SUM=$((SUM + NUMBER))
+done
+
+echo $SUM
