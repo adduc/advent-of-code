8f4d8a867dc51451cfb9500bd2c6daefeca6b270
John Long <john@128.io>
2024-08-08 21:50:39 -0500

    day-2: part-2: add php solution

---
+++ 2023/day-2/part-2/php/cmd.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+$thresholds = [
+    "red" => 12,
+    "green" => 13,
+    "blue" => 14,
+];
+
+$regex = '/(?<count>\d+) (?<color>\w+)/m';
+
+$sum = 0;
+
+while ($line = fgets(STDIN)) {
+
+    $pieces = explode(":", $line);
+
+    $game_id = substr($pieces[0], 5);
+
+    $colors = [];
+
+    preg_match_all($regex, $pieces[1], $matches, PREG_SET_ORDER);
+
+    foreach ($matches as $match) {
+        $colors[$match['color']] = max($colors[$match['color']] ?? 0, $match['count']);
+    }
+
+    $sum += ($colors['red'] * $colors['green'] * $colors['blue']);
+}
+
+fwrite(STDOUT, $sum . PHP_EOL);
