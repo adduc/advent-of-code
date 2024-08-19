38e4299e56778c9305b1d0edf62bbd3978faf972
John Long <john@128.io>
2024-08-19 10:10:17 -0500

    billion-row-challenge: add parallelism for 25 second solution

--- billion-row-challenge/cmd.php
+++ billion-row-challenge/cmd.php
@@ -2,37 +2,115 @@
 <?php declare(strict_types=1);
 
 if ($argc !== 2) {
-    echo "Usage: $argv[0] <file>\n";
+    fwrite(STDERR, "Usage: $argv[0] <file>\n");
     exit(1);
 }
 
-$handle = fopen($argv[1], 'r');
+if (!file_exists($argv[1])) {
+    fwrite(STDERR, "File not found: $argv[1]\n");
+    exit(1);
+}
 
-$stations = [];
+// determine number of cores
+$cores = preg_match_all('/^core id/m', file_get_contents('/proc/cpuinfo'));
 
-while ($line = fgets($handle)) {
-    [$name, $temp] = explode(';', $line);
-    $temp = (float)$temp;
+if ($cores === false) {
+    fwrite(STDERR, "Could not determine number of cores\n");
+    exit(1);
+}
 
-    if (!isset($stations[$name])) {
-        $stations[$name] = [
-            'sum' => $temp,
-            'count' => 1,
-            'min' => $temp,
-            'max' => $temp,
-        ];
-    } else {
-        $stations[$name]['sum'] += $temp;
-        $stations[$name]['count']++;
-        if ($stations[$name]['min'] > $temp) {
-            $stations[$name]['min'] = $temp;
-        } elseif ($stations[$name]['max'] < $temp) {
-            $stations[$name]['max'] = $temp;
+$size = filesize($argv[1]);
+$filename = tempnam(sys_get_temp_dir(), 'billion-row-challenge');
+
+// fork processes
+$children = [];
+
+for ($i = 0; $i < $cores; $i++) {
+    $pid = pcntl_fork();
+
+    if ($pid === -1) {
+        fwrite(STDERR, "Could not fork process\n");
+        exit(1);
+    }
+
+    if ($pid === 0) {
+        $pid = getmypid();
+
+        $handle = fopen($argv[1], 'r');
+
+        $stations = [];
+
+        $start_chunk = (int)floor($size / $cores * $i);
+        $end_chunk = (int)floor($size / $cores * ($i + 1));
+
+        fseek($handle, $start_chunk);
+
+        if ($i > 0) {
+            fgets($handle);
+        }
+
+        while ($line = fgets($handle)) {
+            [$name, $temp] = explode(';', $line);
+            $temp = (float)$temp;
+
+            $station = &$stations[$name];
+
+            if ($station === NULL) {
+                $station = [
+                    'sum' => $temp,
+                    'count' => 1,
+                    'min' => $temp,
+                    'max' => $temp,
+                ];
+            } else {
+
+                $station = &$stations[$name];
+
+                $station['sum'] += $temp;
+                $station['count']++;
+                if ($station['min'] > $temp) {
+                    $station['min'] = $temp;
+                } elseif ($station['max'] < $temp) {
+                    $station['max'] = $temp;
+                }
+            }
+
+            if (ftell($handle) >= $end_chunk) {
+                break;
+            }
+        }
+
+        file_put_contents($filename . '.' . $pid, json_encode($stations));
+        exit(0);
+    }
+
+    $children[] = $pid;
+}
+
+$results = [];
+
+// wait for children to finish and collect results
+foreach ($children as $pid) {
+    pcntl_waitpid($pid, $status);
+
+    $stations  = json_decode(file_get_contents($filename . '.' . $pid), true);
+
+    foreach ($stations as $name => $station) {
+        if (!isset($results[$name])) {
+            $results[$name] = $station;
+        } else {
+            $results[$name]['sum'] += $station['sum'];
+            $results[$name]['count'] += $station['count'];
+            if ($results[$name]['min'] > $station['min']) {
+                $results[$name]['min'] = $station['min'];
+            } elseif ($results[$name]['max'] < $station['max']) {
+                $results[$name]['max'] = $station['max'];
+            }
         }
     }
 }
 
-foreach ($stations as $name => $station) {
+foreach ($results as $name => $station) {
     $mean = number_format($station['sum'] / $station['count'], 1);
     echo "{$name};{$station['min']};{$mean};{$station['max']}\n";
 }
